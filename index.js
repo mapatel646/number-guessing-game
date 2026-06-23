6844bdc0c10c246b1b5b9cb31b812a61ff063bb6
mayurpatel <mapatel646@gmail.com>
2026-06-23 14:41:18 -0500

    feat: add random number generation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+console.log("Welcome to the Number-Guessing Game!");
+
+generateRandomNumber();
+
+function generateRandomNumber() {
+    //0 to 100 inclusive
+    randomNum = Math.floor(Math.random() * 101);
+    console.log("Random number generated = " + randomNum);
+}
