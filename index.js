a621e07b408936ed2cb7ba7ecd8b1f6b229d3f85
mayurpatel <mapatel646@gmail.com>
2026-06-25 19:57:55 -0500

    feat: check invalid input guess

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 console.log("Welcome to the Number-Guessing Game!");
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
 // let playAgain = true;
 
 // while(playAgain){
     let target = generateRandomNumber();
-    const readline = require('readline');
-
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
     readUserGuess();
 //}
 
@@ -23,18 +24,23 @@ function generateRandomNumber() {
 async function readUserGuess(){
 
     rl.question("Please enter your first guess (between 1 and 100 (inclusive)): ",(answer) =>{
-        console.log("You guessed: " + answer);
         let guess = parseInt(answer);
 
-        if(guess < target){
-            console.log("Your guess is too low. Try higher!");
+        if(isNaN(guess) || guess < 1 || guess > 100){
+            console.log("Invalid input. Please enter a number between 1 and 100.");
             readUserGuess();
-        } else if(guess > target){
-            console.log("Your guess is too high. Try lower!");
-            readUserGuess();
-        } else {
-            console.log("Congratulations! You guessed the correct number.");
-            rl.close();
+        } else{
+            console.log("You guessed: " + guess);
+            if(guess < target){
+                console.log("Your guess is too low. Try higher!");
+                readUserGuess();
+            } else if(guess > target){
+                console.log("Your guess is too high. Try lower!");
+                readUserGuess();
+            } else {
+                console.log("Congratulations! You guessed the correct number.");
+                rl.close();
+            }
         }
     });
 }
