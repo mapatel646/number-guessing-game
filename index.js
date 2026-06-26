f97400721e2baa7328662f4bd1b7dd31d91027c8
mayurpatel <mapatel646@gmail.com>
2026-06-25 20:19:11 -0500

    feat: add play again option

--- index.js
+++ index.js
@@ -7,40 +7,49 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-// let playAgain = true;
 
-// while(playAgain){
+function startGame() {
     let target = generateRandomNumber();
-    readUserGuess();
-//}
+    readUserGuess(target);
+}
 
 function generateRandomNumber() {
     //1 to 100 inclusive
     let randomNum = Math.floor(Math.random() * 100) + 1;
-    console.log("Random number generated = " + randomNum);
+    console.log("Random number generated!");
     return randomNum;
 }
 
-async function readUserGuess(){
+async function readUserGuess(target){
 
     rl.question("Please enter your first guess (between 1 and 100 (inclusive)): ",(answer) =>{
         let guess = parseInt(answer);
 
         if(isNaN(guess) || guess < 1 || guess > 100){
             console.log("Invalid input. Please enter a number between 1 and 100.");
-            readUserGuess();
+            readUserGuess(target);
         } else{
             console.log("You guessed: " + guess);
             if(guess < target){
                 console.log("Your guess is too low. Try higher!");
-                readUserGuess();
+                readUserGuess(target);
             } else if(guess > target){
                 console.log("Your guess is too high. Try lower!");
-                readUserGuess();
-            } else {
+                readUserGuess(target);
+            } else if(guess === target){
                 console.log("Congratulations! You guessed the correct number.");
-                rl.close();
+
+                rl.question("Would you like to play again? (yes(y) or no(n)): ",(answer) =>{
+                    if(answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y'){
+                        startGame();
+                    } else {
+                        console.log("Thank you for playing! Goodbye!");
+                        rl.close();
+                    }
+                });
             }
         }
     });
 }
+
+startGame();
